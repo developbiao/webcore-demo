2a93600b0106e3375fbae4ab21f8447e4ca7ae6e
developbiao <developbiao@gmail.com>
2022-07-05 21:45:05 +0800

    modify user_controller

--- user_controller.go
+++ user_controller.go
@@ -6,10 +6,9 @@ import (
 	"github.com/developbiao/webcore-demo/framework"
 )
 
-func UserLoginController(c *framework.Context) error {
+func UserLoginController(c *framework.Context) {
 	foo, _ := c.QueryString("foo", "def")
 	// Wait 10 seconds end
 	time.Sleep(time.Second * 10)
 	c.SetStatus(200).Json("ok, UserLoginController: " + foo)
-	return nil
 }
